3f32a391c3a45bda8f5bff6a4aa708876aefe7db
eujjong18 <eujjong18@naver.com>
2026-08-17 15:04:18 +0900

    initial commit

---
+++ src/main/java/com/swyp/app7_BE/App7BeApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.app7_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App7BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App7BeApplication.class, args);
+	}
+
+}
